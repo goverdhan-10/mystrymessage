694819bece213818d68325c9f6511a949982ed57
goverdhan-10 <gupta.goverdhan7253@gmail.com>
2025-03-05 00:38:50 +0530

    till lec 11 " worked with otp verification"

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -7,14 +7,14 @@ export async function POST(request:Request) {
     try {
         const {username, code} = await request.json()
         const decodedUsername = decodeURIComponent(username)
-
+        console.log("till here 1")
         const user = await UserModel.findOne({username: decodedUsername})
 
         if(!user){
             return Response.json({
                 success: false,
                 message:"user not found"
-            },{status: 500})
+            },{status: 404})
         }
         const isCodeValid = user.verifyCode === code
         const isCodeNotExpired = new Date(user.verifyCodeExpiry)> new Date()
@@ -24,7 +24,7 @@ export async function POST(request:Request) {
             await user.save()
             return Response.json({
                 success: true,
-                message:"user verified successfully"
+                message:"User verified successfully"
             },{status: 200})
         }
         else if(!isCodeNotExpired){
