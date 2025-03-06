db9f64b81c274e446626c48d08548a90db0d48a6
goverdhan-10 <gupta.goverdhan7253@gmail.com>
2025-03-06 17:22:31 +0530

    debug 4

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -3,8 +3,9 @@ import { authOptions } from "../../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import {User} from 'next-auth'
+import { NextRequest } from "next/server";
 
-export async function DELETE(request: Request, {params}: {params: {messageid: string}}) {
+export async function DELETE(request: NextRequest, {params}: {params: {messageid: string}}) {
     const messageId = params.messageid
     await dbConnect()
     const session = await getServerSession(authOptions);
